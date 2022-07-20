39c1748d00f6f295b999387d6b40efe0eb56dfa0
Bharat goyal <56772219+bharatgl@users.noreply.github.com>
2022-07-21 03:17:56 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
-
-ReactDOM.render(<App />, document.getElementById("root"));
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>React Boilerplate</title>
+</head>
+<body>
+<div id="root">
+</div>
+</html>
